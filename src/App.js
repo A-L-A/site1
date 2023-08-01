18f05ec93dbe2b52c0253b51b54796dec168f8c4
A-L-A <64477195+A-L-A@users.noreply.github.com>
2023-08-01 17:12:37 +0200

    a few edits

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import React from 'react'
+import GlobalStyle from './globalStyles';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+//Pages
+
+import Home from "./pages/Home";
+import Signup from './pages/SignupPage';
+import Pricing from './pages/PricingPage';
 
 function App() {
   return (
-    <div className="App">
-      Test
-    </div>
+    <Router>
+      <GlobalStyle/>
+      <Switch>
+        <Route path="/" exact component={Home} />
+				<Route path="/signup" exact component={Signup} />
+				<Route path="/pricing" exact component={Pricing} />
+      </Switch>
+    </Router>
   );
 }
 
